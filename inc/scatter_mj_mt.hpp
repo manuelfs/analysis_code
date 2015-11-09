75bb291b8e27be0e77143e659683e22ce9f9d7b1
Jae Hyeok Yoo <jaehyeokyoo@gmail.com>
2015-11-09 05:22:00 -0800

    update inc/scatter_mj_mt.hpp for src/scatter_mj_mt_data.cxx

--- inc/scatter_mj_mt.hpp
+++ inc/scatter_mj_mt.hpp
@@ -8,6 +8,7 @@
 #include "TGraph.h"
 #include "TRandom3.h"
 #include "TPaveText.h"
+#include "TH2D.h"
 
 #include "baby_basic.hpp"
 
@@ -16,6 +17,10 @@ std::set<size_t> GetRandomIndices(baby_basic &st, double norm, TRandom3 &rand3);
 void Process(baby_basic &st, TGraph &g, TGraph &g_full,
 	     int color, int marker, int size,
 	     const std::set<size_t> & indices, int nleps = 0);
+
+void Process(baby_basic &st, TGraph &g, TGraph &g_full, TH2D &h,
+	     int color, int marker, int size,
+	     const std::set<size_t> & indices, int nleps = 0, bool isData=true);
 
 std::string GetLabel(const std::string &str, double rho);
 
